ccd9606046425aecdbf5ca45e29b9666516ec8c0
Kirill Savin <krllssrgv@gmail.com>
2022-10-29 00:00:45 +0300

    Second commit

---
+++ js/GetAddInf.js
@@ -0,0 +1,135 @@
+function InfOne() {
+    var Turner = document.getElementById("btn1");
+    var Block = document.getElementById("add_inf_1");
+   
+    if (Block.style.display == "none") {
+
+        Block.style.visibility = "visible";
+        Block.style.display = "block";
+        Block.style.height = "300px";
+        Block.style.width = "100%";
+        Turner.style.transform = "rotate(45deg)";
+
+    } else {
+
+        Block.style.visibility = "hidden";
+        Block.style.display = "none";
+        Turner.style.transform = "rotate(0deg)";
+
+    }
+}
+
+
+
+function InfTwo() {
+    var Turner = document.getElementById("btn2");
+    var Block = document.getElementById("add_inf_2");
+   
+    if (Block.style.display === "none") {
+
+        Block.style.visibility = "visible";
+        Block.style.display = "block";
+        Block.style.height = "300px";
+        Block.style.width = "100%";
+        Turner.style.transform = "rotate(45deg)";
+
+    } else {
+
+        Block.style.visibility = "hidden";
+        Block.style.display = "none";
+        Turner.style.transform = "rotate(0deg)";
+
+    }
+}
+
+
+
+function InfThree() {
+    var Turner = document.getElementById("btn3");
+    var Block = document.getElementById("add_inf_3");
+   
+    if (Block.style.display === "none") {
+
+        Block.style.visibility = "visible";
+        Block.style.display = "block";
+        Block.style.height = "300px";
+        Block.style.width = "100%";
+        Turner.style.transform = "rotate(45deg)";
+
+    } else {
+
+        Block.style.visibility = "hidden";
+        Block.style.display = "none";
+        Turner.style.transform = "rotate(0deg)";
+
+    }
+}
+
+
+
+function InfFour() {
+    var Turner = document.getElementById("btn4");
+    var Block = document.getElementById("add_inf_4");
+   
+    if (Block.style.display === "none") {
+
+        Block.style.visibility = "visible";
+        Block.style.display = "block";
+        Block.style.height = "300px";
+        Block.style.width = "100%";
+        Turner.style.transform = "rotate(45deg)";
+
+    } else {
+
+        Block.style.visibility = "hidden";
+        Block.style.display = "none";
+        Turner.style.transform = "rotate(0deg)";
+
+    }
+}
+
+
+
+function InfFive() {
+    var Turner = document.getElementById("btn5");
+    var Block = document.getElementById("add_inf_5");
+   
+    if (Block.style.display === "none") {
+
+        Block.style.visibility = "visible";
+        Block.style.display = "block";
+        Block.style.height = "300px";
+        Block.style.width = "100%";
+        Turner.style.transform = "rotate(45deg)";
+
+    } else {
+
+        Block.style.visibility = "hidden";
+        Block.style.display = "none";
+        Turner.style.transform = "rotate(0deg)";
+
+    }
+}
+
+
+
+function InfSix() {
+    var Turner = document.getElementById("btn6");
+    var Block = document.getElementById("add_inf_6");
+   
+    if (Block.style.display === "none") {
+
+        Block.style.visibility = "visible";
+        Block.style.display = "block";
+        Block.style.height = "300px";
+        Block.style.width = "100%";
+        Turner.style.transform = "rotate(45deg)";
+
+    } else {
+
+        Block.style.visibility = "hidden";
+        Block.style.display = "none";
+        Turner.style.transform = "rotate(0deg)";
+
+    }
+}
